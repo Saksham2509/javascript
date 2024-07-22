2de248d862009ac23e6da140b7cf884fd4d714d3
Saksham2509 <144423743+Saksham2509@users.noreply.github.com>
2024-07-22 10:42:10 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=122
+let accountEmail="sakshamkhurana844@gmail.com"
+var accountPass="12345"
+accountCity="jaipur"
+
+// accountId=2
+
+accountEmail="inionnon"
+accoutnPass="2455"
+accountCity="jaipur"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity])
+
