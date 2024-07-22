7a0e7ac62a595698c1db4fc5d0d11ebf9f881751
Saksham2509 <144423743+Saksham2509@users.noreply.github.com>
2024-07-22 11:34:12 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,31 @@
+let score="33abc";
+// score was string 
+
+//console.log(typeof score);
+
+
+//conversion to number
+let valueInNumber =Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abs" =>NaN
+// true => 1; false=0;
+
+let isLogged=1;
+let booleanisLogged=Boolean(isLogged)
+//console.log(booleanisLogged);
+//console.log(typeof booleanisLogged);
+
+
+// 1=> true  0=>false
+// ""=> false
+// "hitesh" => true
+
+let someNumber=33;
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
