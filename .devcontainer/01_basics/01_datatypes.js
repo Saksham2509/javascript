7f250adc8d879edf8e86caefdce6892f30200bb3
Saksham2509 <144423743+Saksham2509@users.noreply.github.com>
2024-07-22 11:08:01 +0000

    container config done

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,21 @@
+"Use strict" ;// treat all js code as newer version
+
+// alert(3+3)  we are using nodejs not the browser
+console.log(3
+    +3);
+    //code readibilty to be good
+console.log("hitesh");
+
+//number=> 2 to power 53
+// bigint (for big no)
+//string=>" "
+// boolean  true/false
+// null =>standalone value
+// undefined value=>
+    // symbol =>unique
+
+
+//object
+console.log(typeof undefined); // undefines (output)
+console.log(typeof null); // object(output)
+
