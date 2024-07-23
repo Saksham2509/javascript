d2a6511754e3c41874ca79434faa88aa745c488a
Saksham2509 <144423743+Saksham2509@users.noreply.github.com>
2024-07-23 10:47:34 +0000

    datatypes summary

---
+++ .devcontainer/01_basics/datatypes.summary.js
@@ -0,0 +1,31 @@
+//Primitive
+
+//7 types: strings,number,boolean,null,undefined,symbol
+// bigint
+
+ const score=100
+ const scorevalue=100.4
+
+ const islogged =false;
+ const outsidetemp=null;
+
+
+ const id= Symbol('123')
+ const anotherid=Symbol('123')
+
+ console.log(id=== anotherid)
+
+// Refrence type(non primitve)
+
+// array , objects, functions
+const heroes =["shaktiman","spider"]
+ let myObj={
+    name:"saksham",
+    age : 22,
+ }
+ 
+
+const myfunction= function(){
+ console.log("Hello World")
+ }
+
